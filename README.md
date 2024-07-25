4afae0cc58ef39a890feff7176af742009071a35
Abigail S. <solomon.abigail.n@gmail.com>
2024-07-24 23:44:24 -0700

    updates to guessing game project
    added functions for attempt_lists, all_attempt_lists and high_score, commented out high score code, added and commented out opening statement about the high score. Changed code to not count inputs outside of 1-10 range as valid attempts. Removed redundant code for non-integer inputs, leaving except line to deal with it.  Add print lines for attempts, attempts_list and all_attempts_list and commented them out.
    
    Created and wrote instructions in README.md

---
+++ README.md
@@ -0,0 +1,25 @@
+# Number Guessing Game - Unit 1 Project for TechDegree
+
+The number guessing game is a simple Python program that allows the user to
+guess a number from 1 to 10. The user inputs their guess and if it's incorrect,
+the user will be given a hint to whether the correct number is higher or lower
+than their guess.
+
+The user can guess over and over again until they get the correct
+answer. The user will then be told how many attempts it took for them to guess
+the correct number and basic statistics related to this round of the guessing game.
+If the user enters a number outside of the 1 to 10 range or an non integer input, they'll
+be given an error message, the input doesn't count to the user's attempt counter
+and they'll be asked to try again and enter a number from 1 to 10.
+
+The user will then be asked if they want to continue playing the game or not. If the
+user enters yes, they can play again with updated statistics at the end of this round.
+If the user enters no, the user will be given a good bye message and the program will
+ends. 
+
+
+
+
+
+
+
--- a-number-guessing-game-v1/guessing_game_wip_2.py
+++ a-number-guessing-game-v1/guessing_game_wip_2.py
@@ -6,45 +6,59 @@ Project 1 - A Number Guessing Game
 
 # Import the random and statistics modules.
 import statistics 
-import random   
+import random
+# Create an empty list of all attempts ever done for high score
+# def all_attempts_list():
+  # global all_attempts
+  # all_atttempts = []
+
+# all_attempts_list()
+# Let's see if putting empty attempts list here makes a difference
+def attempts_list():
+  global attempts_list
+  attempts_list = []
+
+attempts_list()
+
 # Create the start_game function.
 def start_game(): 
     # Write your code inside this function.
-
+    guess = None
+    attempts = 0
+    # high_score = None 
     #   When the program starts, we want to:
     #   ------------------------------------
     #   1. Display an intro/welcome message to the player.
     print("Welcome player to the number guessing game! ")
     #   2. Store a random number as the answer/solution.
     answer = random.randint(1, 10)
+    #   2.5. High-score message to the player.
+    # print(f"Can you beat the current high score of {high_score}?")
+
     #   3. Continuously prompt the player for a guess.
-    guess = 0
-    attempts = 0
-    attempts_list = []
-    all_attempts = []
     while guess != answer:
         try:
             guess = int(input("Guess a number from 1 - 10: "))
-            attempts += 1
             # print(answer)
     #     a. If the guess is greater than the solution, display to the player "It's lower".
-            if guess < answer:
-                print("It's higher. Try again.")
+            if guess < 0 or guess > 10:
+                print("Please enter a number from 1 and 10. Try again.")
                 #     b. If the guess is less than the solution, display to the player "It's higher".
             elif guess > answer:
                 print("It's lower. Try again.")
-            elif guess < 0:
-                print("Please enter a number from 1 and 10. Try again.")
-            elif guess > 10:
-                print("Please enter a number from 1 to 10. Try again.")
-            elif guess != int(guess):
-               print("Input invalid. Please enter a number between 1 and 10. Try again.")
+                attempts += 1
+            elif guess < answer:
+                print("It's higher. Try again.")
+                attempts += 1
                 #   4. Once the guess is correct, stop looping, inform the user they "Got it" and store the number of guesses it took in a list.
             else:
                 print("Got it!")
+                attempts += 1
                 attempts_list.append(attempts)
-                print(attempts)
-                print(attempts_list)
+                # all_attempts_list.append(attempts_list)
+                # print(attempts)
+                # print(attempts_list)
+                # print(all_attempts_list)
                 #   5. Display the following data to the player
                 #     a. How many attempts it took them to get the correct number in this game
                 print("It took you {} attempts to guess the correct number in this game.".format(attempts))
@@ -52,22 +66,21 @@ def start_game():
                 print("Overall Statistics:")
                 #     b. The mean of the saved attempts list
                 average = statistics.mean(attempts_list)
-                print("Mean: {}".format(average))
+                print(f"Mean: {average}")
                 #     c. The median of the saved attempts list
                 median_attempts = statistics.median(attempts_list)
-                print("Median: {}".format(median_attempts))
+                print(f"Median: {median_attempts}")
                 #     d. The mode of the saved attempts list
                 mode_attempts = statistics.mode(attempts_list)
-                print("Mode: {}".format(mode_attempts))
+                print(f"Mode: {mode_attempts}")
     #   6. Prompt the player to play again
                 play_again = input("Would you like to play again? Enter y/n: ")
         #     a. If they decide to play again, start the game loop over.
-                if play_again.lower() == "y":
+                if play_again.lower() == "y" or play_again.lower() == "yes":
                     print("Let's start again!")
                     start_game()
         #     b. If they decide to quit, show them a goodbye message.
-                elif play_again.lower() == "n":
-                    all_attempts.append(attempts_list)
+                elif play_again.lower() == "n" or play_again.lower() == "no":
                     print("Thanks for playing! Good bye!")
                     break                
                 else:
@@ -79,7 +92,12 @@ def start_game():
         
                     
 # ( You can add more features/enhancements if you'd like to. )
-#high_score = min(all_attempts)
+#   high score
+# def high_score():
+    # global high_score
+    # high_score = min(attempts_list)
+    
+# high_score()
 
 # Kick off the program by calling the start_game function.
 start_game()
